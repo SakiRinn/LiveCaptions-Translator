1fd815b476631f7cbcfd99183cd09eed080c89c0
Humphrey Sun <namesblanks@gmail.com>
2024-11-03 04:05:08 +0800

    feat: display translation history

---
+++ src/HistoryPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using LiveCaptionsTranslator.controllers;
+using LiveCaptionsTranslator.models;
+
+namespace LiveCaptionsTranslator
+{
+    public partial class HistoryPage : Page
+    {
+        public HistoryPage()
+        {
+            InitializeComponent();
+            LoadHistoryAsync();
+            
+            TranslationController.TranslationLogged += async () => await LoadHistoryAsync();
+        }
+
+        private async Task LoadHistoryAsync()
+        {
+            List<TranslationHistoryEntry> history = await SQLiteHistoryLogger.LoadHistoryAsync();
+            
+            await Dispatcher.InvokeAsync(() =>
+            {
+                HistoryDataGrid.ItemsSource = history;
+            });
+        }
+    }
+}
