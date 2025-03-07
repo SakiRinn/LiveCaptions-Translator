19bc7dbe769fda3dae9e718df21002f112bb6fe9
chaixshot <chai5142xshot@gmail.com>
2025-03-08 05:19:58 +0700

    Fixed LiveCaption.exe not restart properly

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -31,8 +31,6 @@ namespace LiveCaptionsTranslator
             AppDomain.CurrentDomain.ProcessExit += OnProcessExit;
 
             window = LiveCaptionsHandler.LaunchLiveCaptions();
-            LiveCaptionsHandler.FixLiveCaptions(window);
-            LiveCaptionsHandler.HideLiveCaptions(window);
 
             caption = Caption.GetInstance();
             setting = Setting.Load();
