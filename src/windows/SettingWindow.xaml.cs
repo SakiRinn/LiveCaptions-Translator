4b145663a562285b1f5cf81ac4941348cd5e7d5a
Humphrey Sun <namesblanks@gmail.com>
2025-03-18 14:56:25 +0800

    feat: enhance SettingWindow with navigation bar

--- src/windows/SettingWindow.xaml.cs
+++ src/windows/SettingWindow.xaml.cs
@@ -1,3 +1,5 @@
+using System.Windows;
+using System.Windows.Media;
 using Wpf.Ui.Appearance;
 using Wpf.Ui.Controls;
 
@@ -5,21 +7,64 @@ namespace LiveCaptionsTranslator
 {
     public partial class SettingWindow : FluentWindow
     {
+        private System.Windows.Controls.Button currentSelected;
+        private Dictionary<string, FrameworkElement> sectionReferences;
+
         public SettingWindow()
         {
             InitializeComponent();
             ApplicationThemeManager.ApplySystemTheme();
             DataContext = Translator.Setting;
-
+            
             Loaded += (sender, args) =>
             {
-                SystemThemeWatcher.Watch(
-                    this,
-                    WindowBackdropType.Mica,
-                    true
-                );
+                SystemThemeWatcher.Watch(this, WindowBackdropType.Mica, true);
+                
+                InitializeSectionReferences();
+
+                SelectButton(GeneralButton);
             };
+        }
+
+        private void InitializeSectionReferences()
+        {
+            sectionReferences = new Dictionary<string, FrameworkElement>
+            {
+                { "General", ContentPanel }, // 通用部分就是整个内容面板
+                { "Prompt", PromptSection },
+                { "Ollama", OllamaSection },
+                { "OpenAI", OpenAISection },
+                { "OpenRouter", OpenRouterSection },
+                { "DeepL", DeepLSection }
+            };
+        }
+
+        private void NavigationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is System.Windows.Controls.Button button)
+            {
+                SelectButton(button);
+
+                string targetSection = button.Tag.ToString();
+
+                if (sectionReferences.TryGetValue(targetSection, out FrameworkElement element))
+                {
+                    element.BringIntoView();
+                }
+            }
+        }
+
+        private void SelectButton(System.Windows.Controls.Button button)
+        {
+
+            if (currentSelected != null)
+            {
+                currentSelected.Background = new SolidColorBrush(Colors.Transparent);
+            }
+            
+            button.Background = (Brush)FindResource("ControlFillColorSecondaryBrush");
+
+            currentSelected = button;
         }
     }
 }
-
