6b0cefb7e651e24119c6665a14ac33553efb29a2
SakiRinn <sakirinn@qq.com>
2025-05-19 23:50:21 +0800

    chore: update version to 1.5.1252.1923

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Reflection;
 
 
 [assembly: AssemblyTitle("LiveCaptions Translator")]
-[assembly: AssemblyVersion("1.5.1252.1818")]
-[assembly: AssemblyFileVersion("1.5.1252.1818")]
+[assembly: AssemblyVersion("1.5.1252.1923")]
+[assembly: AssemblyFileVersion("1.5.1252.1923")]
